b6fb17c9a9561a3b3b921b0391dc15a4466fdeb4
Do Quoc Tien <hadesdqt@gmail.com>
2026-02-24 10:27:55 +0700

    feat(auth): implement Redis session validation to revoke access tokens part 14

--- src/modules/auth/strategies/access-jwt.strategy.ts
+++ src/modules/auth/strategies/access-jwt.strategy.ts
@@ -1,22 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import type { Cache } from 'cache-manager';
+import type { Request } from 'express';
+import { createHash } from 'crypto';
 
 @Injectable()
 export class AccessJwtStrategy extends PassportStrategy(
   Strategy,
   'jwt-access',
 ) {
-  constructor(config: ConfigService) {
+  constructor(
+    config: ConfigService,
+    @Inject(CACHE_MANAGER) private cache: Cache,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: config.get<string>('ACCESS_TOKEN_SECRET')!,
+      passReqToCallback: true, //cho phép lấy Request
     });
   }
+  private createDeviceId(req: Request): string {
+    const ip = req.ip;
+    const userAgent = req.headers['user-agent'] ?? '';
 
-  async validate(payload: any) {
+    const raw = `${ip}:${userAgent}`;
+
+    return createHash('sha256').update(raw).digest('hex');
+  }
+
+  async validate(req: Request, payload: any) {
+    const deviceId = this.createDeviceId(req);
+
+    const sessionKey = `session:${payload.sub}:${deviceId}`;
+
+    const session = await this.cache.get<{ jti: string }>(sessionKey);
+
+    if (!session) {
+      throw new UnauthorizedException('Session expired');
+    }
+
+    if (session.jti !== payload.jti) {
+      throw new UnauthorizedException('Invalid session');
+    }
     // payload chính là data đã sign trong AuthService
     return {
       id: payload.sub,
